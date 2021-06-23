bdf06283a5eef7a727642828795b4101abb27c38
veldmannm <marten.veldmann@dzne.de>
2021-06-23 13:28:23 +0200

    adding setup.py to install client in Python env

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 07:49:16 2017
+
+@author: Marten Veldmann
+"""
+
+from setuptools import setup
+
+
+setup(name='ismrmrd_client',
+      version="1.0",
+      description="Client for streaming ISMRMRD data",
+      scripts=['client.py','connection.py','constants.py'],
+      zip_safe=False,
+      )

8c528dee9aecd017ee6cdf945aef0f106d3fa781
mavel101 <marten.veldmann@hhu.de>
2022-04-19 13:54:24 +0200

    remove user parameter for segments

--- pulseq_prot.py
+++ pulseq_prot.py
@@ -91,12 +91,6 @@ def insert_hdr(prot_file, metadata):
         dset_e1.encodingLimits.segment.minimum = prot_e1.encodingLimits.segment.minimum
         dset_e1.encodingLimits.segment.maximum = prot_e1.encodingLimits.segment.maximum
         dset_e1.encodingLimits.segment.center = prot_e1.encodingLimits.segment.center
-    else:
-        # compatibility with older datasets, where the segment encoding limit parameter was not used
-        try:
-            dset_e1.encodingLimits.segment.maximum = prot_hdr.userParameters.userParameterDouble[2].value - 1
-        except:
-            pass
 
     # acceleration
     if prot_e1.parallelImaging is not None:
@@ -229,11 +223,7 @@ def insert_acq(prot_acq, dset_acq, metadata, noncartesian=True, return_basetrj=T
         
         # calculate full number of samples
         nsamples = dset_acq.number_of_samples
-        try:
-            # user parameter is kept for compatibility (see insert_hdr)
-            nsegments = metadata.encoding[0].encodingLimits.segment.maximum + 1
-        except:
-            nsegments = metadata.userParameters.userParameterDouble[2].value
+        nsegments = metadata.encoding[0].encodingLimits.segment.maximum + 1
         nsamples_full = int(nsamples*nsegments+0.5)
         nsamples_max = 65535
         if nsamples_full > nsamples_max:

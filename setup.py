d83dfa33cb47dd37ec36d6b67c79cbf1ddd0518a
veldmannm <marten.veldmann@dzne.de>
2022-04-19 21:24:26 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ Created on Mon Mar 20 07:49:16 2017
 @author: Marten Veldmann
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='ismrmrd_client',
       version="1.0",
       description="Client for streaming ISMRMRD data",
       scripts=['client.py','connection.py','constants.py'],
+      packages=find_packages(exclude=("docker","parameter_maps")),
       install_requires=[
           'numpy',
           'ismrmrd>=1.9.5',

7875d16288dde4c3d8e82a4b05ccb8b3e8ae4d38
veldmannm <marten.veldmann@dzne.de>
2022-04-20 12:37:54 +0200

    remove fov shifting as its done in the sequence in newer Pulseq versions

--- bart_pulseq_spiral_dream.py
+++ bart_pulseq_spiral_dream.py
@@ -1,17 +1,15 @@
 
 import ismrmrd
 import os
-import itertools
 import logging
 import numpy as np
-import base64
 import ctypes
 
 from bart import bart
 from cfft import cfftn, cifftn
 from pulseq_helper import insert_hdr, insert_acq, get_ismrmrd_arrays, read_acqs
 from reco_helper import calculate_prewhitening, apply_prewhitening, calc_rotmat, pcs_to_gcs, remove_os
-from reco_helper import fov_shift_spiral_reapply #, fov_shift_spiral, fov_shift 
+from reco_helper import fov_shift_spiral_reapply
 from reco_helper import filt_ksp
 from DreamMap import calc_fa, DREAM_filter_fid
 
@@ -420,15 +418,6 @@ def process_acs(group, metadata, dmtx=None, gpu=False):
     if len(group)>0:
         data = sort_into_kspace(group, metadata, dmtx, zf_around_center=True)
 
-        #--- FOV shift is done in the Pulseq sequence by tuning the ADC frequency   ---#
-        #--- However leave this code to fall back to reco shifts, if problems occur ---#
-        #--- and for reconstruction of old data                                     ---#
-        # rotmat = calc_rotmat(group[0])
-        # if not rotmat.any(): rotmat = -1*np.eye(3) # compatibility if refscan rotmat is not in protocol, this is the standard Pulseq rotation matrix
-        # res = metadata.encoding[0].encodedSpace.fieldOfView_mm.x / metadata.encoding[0].encodedSpace.matrixSize.x
-        # shift = pcs_to_gcs(np.asarray(group[0].position), rotmat) / res
-        # data = fov_shift(data, shift)
-
         if gpu and data.shape[2]>1: # only use GPU for 3D data, as otherwise the overhead makes it slower than CPU
             sensmaps = bart(1, 'ecalib -g -m 1 -k 6 -I', data)  # ESPIRiT calibration
         else:
@@ -464,13 +453,7 @@ def sort_spiral_data(group, metadata, dmtx=None):
         # update trajectory
         traj = np.swapaxes(acq.traj[:,:3],0,1) # [samples, dims] to [dims, samples]
         trj.append(traj)
-        
-        #--- FOV shift is done in the Pulseq sequence by tuning the ADC frequency   ---#
-        #--- However leave this code to fall back to reco shifts, if problems occur ---#
-        #--- and for reconstruction of old data                                     ---#
-        # shift = pcs_to_gcs(np.asarray(acq.position), rot_mat) / res
-        # sig[-1] = fov_shift_spiral(sig[-1], traj, shift, nx)
-        
+       
     np.save(debugFolder + "/" + "enc.npy", enc)
     
     # convert lists to numpy arrays

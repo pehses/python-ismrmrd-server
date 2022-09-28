change z-coordinates offset sign, as the old definition makes no sense & fix bug that appends data twice if adc map calculation fails

powergrid_pulseq.py
@@ -653,8 +653,6 @@ def process_raw(acqGroup, metadata, sensmaps, shotimgs, prot_arrays, img_coord):
     # We now (in case of diffusion imaging) split the b=0 image from other images and reshape to b-values (contrast) and directions (phase)
     if "b_values" in prot_arrays and not process_raw.reco_n_contr:
         try:
-            # Append data
-            dsets.append(data)
             data_eval = abs(data)
 
             # Reshape Arrays
@@ -670,7 +668,10 @@ def process_raw(acqGroup, metadata, sensmaps, shotimgs, prot_arrays, img_coord):
             b0 = np.expand_dims(b0.mean(1), 1) # averages b=0 scans
             adc_maps = process_diffusion_images(b0, diffw_imgs, prot_arrays, mask)
             adc_maps = adc_maps[:,np.newaxis] # add empty nz dimension for correct flip
+
+            # Append data
             dsets.append(adc_maps)
+            dsets.append(data)
         except:
             logging.debug("ADC map calculation failed.")
             dsets.append(data)

reco_helper.py
@@ -356,7 +356,7 @@ def calc_img_coord(metadata, acq):
     ix = np.linspace(-nx/2*res,(nx/2-1)*res, nx)
     iy = np.linspace(-ny/2*res,(ny/2-1)*res, ny)
     slice_offset = slc_res*(acq.idx.slice-(n_slc-1)/2) # this is the offset of the first slice in a stack from the volumes center
-    iz = -1*(np.linspace(0, (nz-1)*slc_sep, nz) + slice_offset) # switch sign as we measure from head to feet
+    iz = np.linspace(0, (nz-1)*slc_sep, nz) + slice_offset
     grid = np.asarray(np.meshgrid(ix,iy,iz)).reshape([3,-1])
 
     # Coordinates to DCS (physical)
@@ -366,7 +366,7 @@ def calc_img_coord(metadata, acq):
     global_offset = 1e-3 * pcs_to_dcs(np.asarray(acq.position)) # [m] -> [mm]
     grid_rot[0] -= global_offset[0]
     grid_rot[1] -= global_offset[1]
-    grid_rot[2] += global_offset[2] # also switch sign here
+    grid_rot[2] += global_offset[2]
 
     # reshape back
     grid_rot = grid_rot.reshape([3,len(ix),len(iy),len(iz)])
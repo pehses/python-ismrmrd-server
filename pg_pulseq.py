5d5d85622efa1b67dfc692eabcd164d3308bac4a
mavel101 <marten.veldmann@hhu.de>
2023-01-03 10:05:56 +0100

    higher order recon selectable from metadata file

--- pg_pulseq.py
+++ pg_pulseq.py
@@ -48,7 +48,7 @@ def process(connection, config, metadata):
         importlib.reload(powergrid_pulseq_dream)
         logging.info("Starting PowerGrid 3DREAM reconstruction.")
         powergrid_pulseq_dream.process(connection, config, metadata, prot_file)
-    elif len(metadata.userParameters.userParameterBase64):
+    elif len(metadata.userParameters.userParameterBase64) or len(prot_hdr.userParameters.userParameterBase64):
         importlib.reload(powergrid_pulseq_ho)
         logging.info("Starting PowerGrid spiral higher order reconstruction.")
         powergrid_pulseq_ho.process(connection, config, metadata, prot_file)

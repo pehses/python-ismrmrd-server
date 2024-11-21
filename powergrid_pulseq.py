changes for default gradient orientations of newer Pulseq interpreters, change definition of compressed coils, fix a bug in the cartesian reco, if no fieldmap is reconstructed

powergrid_pulseq.py
@@ -43,6 +43,8 @@ read_ecalib = False
 save_cmplx = True # save images as complex data
 online_recon = False
 
+compressed_coils = None
+
 ########################
 # Main Function
 ########################
@@ -64,15 +66,16 @@ def process(connection, config, metadata, prot_file):
         global online_recon
         online_recon = True
 
-    # Coil Compression: Compress number of coils by n_compr coils
-    n_compr = 0
+    # Coil Compression
     n_cha = metadata.acquisitionSystemInformation.receiverChannels
-    if n_compr > 0 and n_compr<n_cha:
-        process_acs.cc_cha = n_cha - n_compr
-        logging.debug(f'Coil Compression from {n_cha} to {process_acs.cc_cha} channels.')
-    elif n_compr<0 or n_compr>=n_cha:
-        process_acs.cc_cha = n_cha
-        logging.debug('Invalid number of compressed coils.')
+    global compressed_coils
+    if compressed_coils is not None:
+        if compressed_coils > 0 and compressed_coils<=n_cha:
+            process_acs.cc_cha = compressed_coils
+            logging.debug(f'Coil Compression from {n_cha} to {process_acs.cc_cha} channels.')
+        else:
+            process_acs.cc_cha = n_cha
+            logging.debug('Invalid number of compressed coils. Set back to original number of coils.')
     else:
         process_acs.cc_cha = n_cha
 
@@ -98,6 +101,7 @@ def process(connection, config, metadata, prot_file):
 
     # Read user parameters
     up_double = {item.name: item.value for item in metadata.userParameters.userParameterDouble}
+    up_base = {item.name: item.value for item in metadata.userParameters.userParameterBase64}
 
     # Check SMS, in the 3D case we can have an acceleration factor, but its not SMS
     sms_factor = int(metadata.encoding[0].parallelImaging.accelerationFactor.kspace_encoding_step_2) if metadata.encoding[0].encodingLimits.slice.maximum > 0 else 1
@@ -297,6 +301,8 @@ def process(connection, config, metadata, prot_file):
                     shift = rh.pcs_to_gcs(np.asarray(last_item.position), rotmat) # shift [mm] in GCS, as traj is in GCS
                     shift_px = shift / res # shift in pixel
                     traj = last_item.traj[:,:3]
+                    if "spiral_nopos" in up_base and up_base["spiral_nopos"] == "1":
+                        base_trj = None
                     last_item.data[:] = rh.fov_shift_spiral_reapply(last_item.data[:], traj, base_trj, shift_px, matr_sz)
 
                     # filter signal to avoid Gibbs Ringing

powergrid_pulseq_ho.py
@@ -55,6 +55,8 @@ n_replica = 50 # number of replicas used for SNR map calculation
 reco_n_contr = 0 # if >0 only the volumes up to the specified number will be reconstructed
 first_vol = 0 # index of first volume, that is reconstructed
 
+compressed_coils = None # if not None compress number of coils to the specified number of coils
+
 ########################
 # Main Function
 ########################
@@ -86,15 +88,16 @@ def process(connection, config, metadata, prot_file):
         read_ecalib = False
         read_fmap = False
 
-    # Coil Compression: Compress number of coils by n_compr coils
-    n_compr = 0
+    # Coil Compression
     n_cha = metadata.acquisitionSystemInformation.receiverChannels
-    if n_compr > 0 and n_compr<n_cha:
-        process_acs.cc_cha = n_cha - n_compr
-        logging.debug(f'Coil Compression from {n_cha} to {process_acs.cc_cha} channels.')
-    elif n_compr<0 or n_compr>=n_cha:
-        process_acs.cc_cha = n_cha
-        logging.debug('Invalid number of compressed coils.')
+    global compressed_coils
+    if compressed_coils is not None:
+        if compressed_coils > 0 and compressed_coils<=n_cha:
+            process_acs.cc_cha = compressed_coils
+            logging.debug(f'Coil Compression from {n_cha} to {process_acs.cc_cha} channels.')
+        else:
+            process_acs.cc_cha = n_cha
+            logging.debug('Invalid number of compressed coils. Set back to original number of coils.')
     else:
         process_acs.cc_cha = n_cha
 
@@ -124,6 +127,7 @@ def process(connection, config, metadata, prot_file):
 
     # Read user parameters
     up_double = {item.name: item.value for item in metadata.userParameters.userParameterDouble}
+    up_base = {item.name: item.value for item in metadata.userParameters.userParameterBase64}
 
     # Check SMS, in the 3D case we can have an acceleration factor, but its not SMS
     sms_factor = int(metadata.encoding[0].parallelImaging.accelerationFactor.kspace_encoding_step_2) if metadata.encoding[0].encodingLimits.slice.maximum > 0 else 1
@@ -283,8 +287,9 @@ def process(connection, config, metadata, prot_file):
                     rh.apply_cc(item, process_acs.cc_mat)
 
                 # Undo FOV Shift with base trajectory
-                shift = rh.pcs_to_dcs(np.asarray(item.position)) * 1e-3 # shift [m] in DCS
-                item.data[:] *= np.exp(1j*(shift*base_trj).sum(axis=-1)) # base_trj in [rad/m]
+                if "spiral_nopos" not in up_base or up_base["spiral_nopos"] != "1":
+                    shift = rh.pcs_to_dcs(np.asarray(item.position)) * 1e-3 # shift [m] in DCS
+                    item.data[:] *= np.exp(1j*(shift*base_trj).sum(axis=-1)) # base_trj in [rad/m]
 
                 # Correct the global phase
                 k0 = item.traj[:,3]

pulseq_helper.py
@@ -3,6 +3,7 @@
 Includes trajectory prediction with the GIRF
 """
 
+from packaging.version import Version
 import ismrmrd
 import h5py
 import numpy as np
@@ -42,10 +43,12 @@ def insert_hdr(prot_file, metadata):
     if prot_hdr.userParameters is not None:
         dset_up = [metadata.userParameters.userParameterDouble,
                    metadata.userParameters.userParameterLong,
-                   metadata.userParameters.userParameterBase64]
+                   metadata.userParameters.userParameterBase64,
+                   metadata.userParameters.userParameterString]
         prot_up = [prot_hdr.userParameters.userParameterDouble,
                    prot_hdr.userParameters.userParameterLong,
-                   prot_hdr.userParameters.userParameterBase64]
+                   prot_hdr.userParameters.userParameterBase64,
+                   prot_hdr.userParameters.userParameterString]
         for i in range(len(dset_up)):
             dset_up_dict = {item.name: item.value for item in dset_up[i]}
             prot_up_dict = {item.name: item.value for item in prot_up[i]}
@@ -58,6 +61,8 @@ def insert_hdr(prot_file, metadata):
                     up = ismrmrd.xsd.userParameterLongType(name=key, value=merged_dict[key])
                 elif i == 2:
                     up = ismrmrd.xsd.userParameterBase64Type(name=key, value=merged_dict[key])
+                elif i == 3:
+                    up = ismrmrd.xsd.userParameterStringType(name=key, value=merged_dict[key])
                 dset_up[i].append(up)
 
     # encoding
@@ -203,9 +208,17 @@ def insert_acq(prot_acq, dset_acq, metadata, noncartesian=True, return_basetrj=T
     # # Process acquisition
     # #---------------------------
 
-    # convert positions for correct rotation matrix - this was experimentally validated on 20210709
-    # In Pulseq interpreter v1.4 this is valid for the "old/compat" FOV positioning mode
-    # Shifts and rotations in diffent directions lead to correctly shifted/rotated images and trajectories
+    # The rotation matrix below was experimentally validated for the "old/compat" FOV positioning mode of the Pulseq interpreter.
+    # All reconstruction scripts are based on this mode, which however is deprecated from Pulseq 1.4.0 onwards.
+    # The new default FOV positioning mode ("XYZ in TRA") correctly maps the axes defined in Pulseq to the physical coordinate system
+    # This is different from the "old/compat" mode by a sign flip in the x-axis.
+    # To make the reconstruction scripts compatible with the new mode, the data or trajectories are now flipped below.
+    prot_up_string = {item.name: item.value for item in metadata.userParameters.userParameterString}
+    vers = Version("1.3.1")
+    if "pulseq_version" in prot_up_string:
+        vers = Version(prot_up_string["pulseq_version"])
+
+    # Convert positions for correct rotation matrix
     tmp = -1* np.asarray(dset_acq.phase_dir[:])
     dset_acq.phase_dir[:] = np.asarray(dset_acq.read_dir[:])
     dset_acq.read_dir[:] = tmp
@@ -227,10 +240,16 @@ def insert_acq(prot_acq, dset_acq, metadata, noncartesian=True, return_basetrj=T
     dset_acq.user_float[:] = prot_acq.user_float[:]
 
     # flags
+    if vers >= Version("1.4.0"):
+        # If NOPOS or NOROT labels are used in the Pulseq sequence, all labels in the acquisition data are messed up (incl ACQ_LAST_IN_MEASUREMENT)
+        # Therefore only the labels from the protocol file will be used here
+        dset_acq.clear_all_flags()
     if prot_acq.is_flag_set(ismrmrd.ACQ_LAST_IN_SLICE):
         dset_acq.setFlag(ismrmrd.ACQ_LAST_IN_SLICE)
     if prot_acq.is_flag_set(ismrmrd.ACQ_LAST_IN_REPETITION):
         dset_acq.setFlag(ismrmrd.ACQ_LAST_IN_REPETITION)
+    if prot_acq.is_flag_set(ismrmrd.ACQ_LAST_IN_MEASUREMENT):
+        dset_acq.setFlag(ismrmrd.ACQ_LAST_IN_MEASUREMENT)
     if prot_acq.is_flag_set(ismrmrd.ACQ_IS_REVERSE):
         dset_acq.setFlag(ismrmrd.ACQ_IS_REVERSE)
     if prot_acq.is_flag_set(ismrmrd.ACQ_IS_NOISE_MEASUREMENT):
@@ -248,7 +267,12 @@ def insert_acq(prot_acq, dset_acq, metadata, noncartesian=True, return_basetrj=T
         dset_acq.resize(trajectory_dimensions=prot_acq.traj[:].shape[1], number_of_samples=dset_acq.number_of_samples, active_channels=dset_acq.active_channels)
         if dset_acq.traj.shape[-1] > 0:
             dset_acq.traj[:] = prot_acq.traj[:]
+        elif vers >= Version("1.4.0"):
+            dset_acq.data[:] = dset_acq.data[:,::-1]
         return
+
+    if not noncartesian and vers >= Version("1.4.0"):
+        dset_acq.data[:] = dset_acq.data[:,::-1]
 
     # deal with noncartesian trajectories
     base_trj = None
@@ -276,7 +300,7 @@ def insert_acq(prot_acq, dset_acq, metadata, noncartesian=True, return_basetrj=T
         data_tmp = dset_acq.data[:].copy()
         traj_tmp = dset_acq.traj[:].copy()
         if dset_acq.traj.size > 0:
-            # trajectory already inserted from Skope system
+            # trajectory inserted from Skope system in physical coordinates
             base_trj = calc_traj(prot_acq, metadata, nsamples_full, rotmat, use_girf=False, traj_phys=traj_phys)[0] # for FOV shift reapply
             if dset_acq.traj.shape[1] < 4:
                 dset_acq.resize(trajectory_dimensions=4, number_of_samples=nsamples_full, active_channels=dset_acq.active_channels)
@@ -284,6 +308,8 @@ def insert_acq(prot_acq, dset_acq, metadata, noncartesian=True, return_basetrj=T
                 dset_acq.traj[:,:3] = traj_tmp
                 dset_acq.traj[:,3] = np.zeros(len(dset_acq.traj[:])) # add zeros, if k0 from Skope not inserted
         else:
+            if vers >= Version("1.4.0"):
+                prot_acq.traj[:,0] *= -1
             dset_acq.resize(trajectory_dimensions=4, number_of_samples=nsamples_full, active_channels=dset_acq.active_channels)
             if prot_acq.traj.shape[0] == dset_acq.data.shape[1] and prot_acq.traj[:,:3].max() > 1:
                 # trajectory in protocol file

reco_helper.py
@@ -297,35 +297,33 @@ def fov_shift_spiral(sig, trj, shift, matr_sz):
 
 def fov_shift_spiral_reapply(sig, pred_trj, base_trj, shift, matr_sz):
     """ 
-    Re-apply FOV shift on spiral/noncartesian data, when FOV positioning in the Pulseq sequence is enabled
+    Re-apply (2D) FOV shift on spiral/noncartesian data, when FOV positioning in the Pulseq sequence is enabled
     first undo field of view shift with nominal, then reapply with predicted trajectory
 
     IMPORTANT: The nominal trajectory has to be shifted by -10us as the ADC frequency adjustment
                of the scanner is lagging behind be one gradient raster time (10 us).
                For Pulseq sequences this is done in pulseq_helper.py
 
+    From Pulseq 1.4.2 on it is possible to disable FOV positioning for single blocks (e.g. spirals).
+    In this case the nominal trajectory should be set to None or zeros.
+
     sig: signal data (dimensions as in ISMRMRD [coils, samples]) 
-    pred_traj: predicted trajectory (dimensions as in ISMRMRD [samples, dims]) 
-    base_trj: nominal trajectory (dimensions as in ISMRMRD [samples, dims])
-    shift: shift [x_shift, y_shift] in voxel
+    pred_traj: predicted/measured trajectory (dimensions as in ISMRMRD [samples, dims]) in logical coordinate system
+    base_trj: nominal trajectory (dimensions as in ISMRMRD [samples, dims]) in logical coordinate system
+              if None, only apply the shift with the predicted/measured trajectory
+    shift: shift [x_shift, y_shift] in voxel in logical coordinate system
     matr_sz: matrix size [x,y]
     """
-
-    pred_trj = pred_trj[:,:2]
-    base_trj = base_trj[:,:2]
-
-    if (abs(shift[0]) < 1e-2) and (abs(shift[1]) < 1e-2):
-        # nothing to do
-        return sig
 
     kmax = (matr_sz/2+0.5).astype(np.int32)[:2]
     shift = shift[:2]
 
     # undo FOV shift from nominal traj
-    sig *= np.exp(1j*np.pi*(shift*base_trj/kmax).sum(axis=-1))
+    if base_trj is not None:
+        sig *= np.exp(1j*np.pi*(shift*base_trj[:,:2]/kmax).sum(axis=-1))
 
     # redo FOV shift with predicted traj
-    sig *= np.exp(-1j*np.pi*(shift*pred_trj/kmax).sum(axis=-1))
+    sig *= np.exp(-1j*np.pi*(shift*pred_trj[:,:2]/kmax).sum(axis=-1))
 
     return sig
 
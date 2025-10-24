fix prewhitening and snr calculation, add scaling factor for spiral pics as sometimes the regularization is different across slices

bart_pulseq_spiral.py
@@ -134,6 +134,9 @@ def process_spiral(connection, config, metadata, prot_file):
 
     base_trj = None
 
+    # Oversampling factor
+    os_factor = up_double["os_factor"] if "os_factor" in up_double else 1
+
     # read protocol acquisitions - faster than doing it one by one
     logging.debug("Reading in protocol acquisitions.")
     acqs = read_acqs(prot_file)
@@ -163,7 +166,7 @@ def process_spiral(connection, config, metadata, prot_file):
                         noise_data.append(acq.data)
                     noise_data = np.concatenate(noise_data, axis=1)
                     # calculate pre-whitening matrix
-                    dmtx = rh.calculate_prewhitening(noise_data)
+                    dmtx = rh.calculate_prewhitening(noise_data, scale_factor=os_factor, os_removed=False)
                     del(noise_data)
                     noiseGroup.clear()
                     
@@ -215,7 +218,6 @@ def process_spiral(connection, config, metadata, prot_file):
                     last_item.data[:] = rh.fov_shift_spiral_reapply(last_item.data[:], pred_trj, base_trj, shift, matr_sz)
 
                     # remove oversampling
-                    os_factor = up_double["os_factor"] if "os_factor" in up_double else 1
                     if os_factor == 2:
                         rh.remove_os_spiral(last_item)
 
@@ -310,14 +312,17 @@ def process_raw(group, metadata, cc_cha, dmtx=None, sensmaps=None, gpu=False, pa
     sms_factor = int(metadata.encoding[0].parallelImaging.accelerationFactor.kspace_encoding_step_2) if metadata.encoding[0].encodingLimits.slice.maximum > 0 else 1
     slc_res = metadata.encoding[0].encodedSpace.fieldOfView_mm.z
 
+    scale_fac_pics = 1500
     if gpu and nz>1: # only use GPU for 3D data, as otherwise the overhead makes it slower than CPU
         nufft_config = 'nufft -g -i -m 15 -l 0.005 -t -d %d:%d:%d'%(nx, nx, nz)
         ecalib_config = 'ecalib -g -m 1 -I'
-        pics_config = 'pics -g -S -e -l1 -r 0.001 -i 50'
+        pics_config = f'pics -g -S -e -l1 -r 0.001 -i 50'
     else:
         nufft_config = 'nufft -i -m 15 -l 0.005 -t -d %d:%d:%d'%(nx, nx, nz)
         ecalib_config = 'ecalib -m 1 -I'
-        pics_config = 'pics -S -e -l1 -r 0.001 -i 50'
+        pics_config = f'pics -S -e -l1 -r 0.001 -i 50'
+    if scale_fac_pics is not None:
+        pics_config += f' -w {scale_fac_pics}'
 
     if parallel:
         ksp = []
@@ -398,8 +403,8 @@ def process_raw(group, metadata, cc_cha, dmtx=None, sensmaps=None, gpu=False, pa
                     data_snr.append(img_snr)
                 else:
                     data_snr.append(bart(1, pics_config, ksp_noise, sensmaps, t=traj))
-            data_snr = np.abs(np.asarray(data_snr))
-            std_dev = np.std(data_snr + np.max(data_snr), axis=0)
+            data_snr = np.asarray(data_snr)
+            std_dev = np.std(np.abs(data_snr + np.max(np.abs(data_snr))), axis=0)
             snr = np.divide(np.abs(data), std_dev, where=std_dev!=0, out=np.zeros_like(std_dev))
             snr = np.swapaxes(snr, 0, 1)
             snr = np.flip(snr, (0,1,2))

powergrid_epi_ho.py
@@ -164,8 +164,8 @@ def process(connection, config, metadata):
                         noise_data.append(acq.data[:])
                     noise_data = np.concatenate(noise_data, axis=1)
                     # calculate pre-whitening matrix
-                    noise_scale = dwell_acq / dwell_noise * 0.793 # factor 0.793 considers filtered area in ADC, see Kellman, 2005, value is fixed for Siemens scanner
-                    dmtx = rh.calculate_prewhitening(noise_data, scale_factor=noise_scale)
+                    noise_scale = dwell_acq / dwell_noise
+                    dmtx = rh.calculate_prewhitening(noise_data, scale_factor=noise_scale, os_removed=False)
                     del(noise_data)
                     noiseGroup.clear()
                                
@@ -428,13 +428,13 @@ def process_raw(acqGroup, metadata, img_coord):
                     subprocess.run('echo quit | nvidia-cuda-mps-control', shell=True) 
                 logging.debug(e.stdout)
                 raise RuntimeError("PowerGrid Reconstruction failed. See logfiles for errors.")
-            data_snr = abs(np.load(os.path.join(tempdir,"images_pg.npy")))
+            data_snr = np.load(os.path.join(tempdir,"images_pg.npy"))
             data_snr = np.transpose(data_snr, [3,4,2,1,0,5,6,7]).mean(axis=0)
             data_snr_list.append(data_snr.reshape(newshape, order='f'))
         data_snr = np.array(data_snr_list)
 
         # calculate SNR maps
-        std_dev = np.std(data_snr + np.max(data_snr), axis=0)
+        std_dev = np.std(np.abs(data_snr + np.max(np.abs(data_snr))), axis=0)
         snr = np.divide(abs(data), std_dev, where=std_dev!=0, out=np.zeros_like(std_dev))
         snr = snr[0,0,0]
         dsets['snr_map'] = snr

powergrid_pulseq_dream.py
@@ -114,6 +114,9 @@ def process(connection, config, metadata, prot_file):
     except:
         logging.info("Improperly formatted metadata: \n%s", metadata)
 
+    # Oversampling factor
+    os_factor = up_double["os_factor"] if "os_factor" in up_double else 1
+
     # Log some measurement parameters
     freq = metadata.experimentalConditions.H1resonanceFrequency_Hz
     shim_currents = [v for k,v in up_double.items() if "ShimCurrent" in k]
@@ -171,7 +174,7 @@ def process(connection, config, metadata, prot_file):
                         noise_data.append(acq.data)
                     noise_data = np.concatenate(noise_data, axis=1)
                     # calculate pre-whitening matrix
-                    dmtx = rh.calculate_prewhitening(noise_data)
+                    dmtx = rh.calculate_prewhitening(noise_data, scale_factor=os_factor, os_removed=False)
                     del(noise_data)
                     noiseGroup.clear()
                                
@@ -219,7 +222,6 @@ def process(connection, config, metadata, prot_file):
                 item.data[:] = rh.filt_ksp(item.data[:], traj, filt_fac=0.95)
 
                 # remove ADC oversampling
-                os_factor = up_double["os_factor"] if "os_factor" in up_double else 1
                 if os_factor == 2:
                     rh.remove_os_spiral(item)
                 

powergrid_pulseq_ho.py
@@ -241,8 +241,8 @@ def process(connection, config, metadata, prot_file):
                     for acq in noiseGroup:
                         noise_data.append(acq.data[:])
                     noise_data = np.concatenate(noise_data, axis=1)
-                    # calculate pre-whitening matrix - noise data has same dwelltime as spiral scan, but oversampling is not removed, therefore add os_factor to scale_facotr
-                    dmtx = rh.calculate_prewhitening(noise_data, scale_factor=os_factor*0.793) # scale factor considers filtered area in ADC, see Kellman, 2005, value is fixed for Siemens scanner
+                    # calculate pre-whitening matrix - noise data has same dwelltime as spiral scan, but oversampling is not removed, therefore add os_factor to scale_factor
+                    dmtx = rh.calculate_prewhitening(noise_data, scale_factor=os_factor, os_removed=False)
                     del(noise_data)
                     noiseGroup.clear()
                                
@@ -657,13 +657,13 @@ def process_raw(acqGroup, metadata, acs, prot_arrays, img_coord, online_recon=Fa
             finally:
                 if mps_server:
                     subprocess.run('echo quit | nvidia-cuda-mps-control', shell=True)
-            data_snr = abs(np.load(os.path.join(tempdir,"images_pg.npy")))
+            data_snr = np.load(os.path.join(tempdir,"images_pg.npy"))
             data_snr = np.transpose(data_snr, [3,4,2,1,0,5,6,7]).mean(axis=0)
             data_snr_list.append(data_snr.reshape(newshape, order='f'))
         data_snr = np.array(data_snr_list)
 
         # calculate SNR maps
-        std_dev = np.std(data_snr + np.max(data_snr), axis=0)
+        std_dev = np.std(np.abs(data_snr + np.max(np.abs(data_snr))), axis=0)
         snr = np.divide(abs(data), std_dev, where=std_dev!=0, out=np.zeros_like(std_dev))
         snr = snr[0,0,0]
         dsets['snr_map'] = snr

reco_helper.py
@@ -46,7 +46,7 @@ def read_cplx_mat_file(filename, key='imgs'):
 
 ## Noise-prewhitening
 
-def calculate_prewhitening(noise, scale_factor=1.):
+def calculate_prewhitening(noise, scale_factor=1., os_removed=True):
     '''Calculates the noise prewhitening matrix
 
     :param noise: Input noise data (2D array), ``[coil, nsamples]``
@@ -57,6 +57,9 @@ def calculate_prewhitening(noise, scale_factor=1.):
 
     :returns w: Prewhitening matrix, ``[coil, coil]``, w @ data is prewhitened
     '''
+    if not os_removed:
+        scale_factor *= 0.793 # considers filtered area in ADC, see Kellman, 2005, value is fixed for Siemens scanner
+
     dmtx = (noise @ np.conj(noise).T)/(noise.shape[1]-1)
     dmtx = np.linalg.inv(np.linalg.cholesky(dmtx))
     dmtx *= np.sqrt(2)*np.sqrt(scale_factor)